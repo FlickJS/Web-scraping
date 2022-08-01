914445a88dcfd8479b6d0e5518f2a65a66123597
Filip Burski <filip.burski@gogomedia.pl>
2022-08-01 19:21:47 +0200

    insert functions into one big class

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let getItems = JSON.parse(window.localStorage.getItem("element"));
 let data;
-
 class Item {
   constructor(website, title, price, brand, image) {
     this.website = website;
@@ -9,32 +8,35 @@ class Item {
     this.brand = brand;
     this.image = image;
   }
+};
+class ScrapeWeb extends Item {
+  constructor(website, title, price, brand, image) {
+    super(website, title, price, brand, image);
+    this._scraping();
+  }
+  _getData = () => {
+    getItems ? data = {products: [...getItems.products]} : data = {products: []};
+    const website = document.location.origin + document.location.pathname;
+    const title = document.querySelector("meta[property='og:title']").getAttribute("content");
+    const price = document.querySelector("meta[property='product:price:amount']").getAttribute("content");
+    const brand = document.querySelector("meta[property='product:brand']").getAttribute("content");
+    const image = document.querySelector("meta[name='og:image']").getAttribute("content");
+    const metaTags = [website, title, price, brand, image];
+    return metaTags;
+  }
+  _savaData = (metaTags) => {
+    for (let i = 0; i < metaTags.length; i++) {
+      if (metaTags[i] === "") {
+        console.error("Missing data");
+      }
+    }
+    data.products.push(new Item(...metaTags));
+    window.localStorage.setItem("element", JSON.stringify(data));
+  }
+  _scraping = () => {
+    const metaTags = this._getData();
+    this._savaData(metaTags);
+  };
 }
 
-const getData = () => {
-  getItems ? data = {products: [...getItems.products]} : data = {products: []};
-  const website = document.location.origin + document.location.pathname;
-  const title = document.querySelector("meta[property='og:title']").getAttribute("content");
-  const price = document.querySelector("meta[property='product:price:amount']").getAttribute("content");
-  const brand = document.querySelector("meta[property='product:brand']").getAttribute("content");
-  const image = document.querySelector("meta[name='og:image']").getAttribute("content");
-  const metaTags = [website, title, price, brand, image];
-  return metaTags;
-};
-
-const savaData = (metaTags) => {
-  for (let i = 0; i < metaTags.length; i++) {
-    if (metaTags[i] === "") {
-      console.error("Missing data");
-    }
-  }
-  data.products.push(new Item(...metaTags));
-  window.localStorage.setItem("element", JSON.stringify(data));
-};
-
-const scraping = () => {
-  const metaTags = getData();
-  savaData(metaTags);
-};
-
-scraping();
+const init = new ScrapeWeb();
